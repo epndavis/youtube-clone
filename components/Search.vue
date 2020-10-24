7b62d1fe4b9cf221d91a4a0ae6d6c02dff4c5579
epndavis <47059262+epndavis@users.noreply.github.com>
2020-10-24 22:13:47 +0100

    feat(search): Add keyboard shortcut for search bar

--- components/Search.vue
+++ components/Search.vue
@@ -1,7 +1,7 @@
 <template>
     <form @submit.prevent="search">
         <div class="search-bar flex items-center items-stretch w-full max-w-2xl m-auto">
-            <input v-model="query" class="px-3 py-1 text-md border border-r-0 rounded-l-sm outline-none flex-1" type="text" placeholder="Search">
+            <input ref="search" v-model="query" class="px-3 py-1 text-md border border-r-0 rounded-l-sm outline-none flex-1" type="text" placeholder="Search">
 
             <button class="bg-gray-100 hover:bg-gray-200 w-16 border text-gray-600 hover:text-gray-800 rounded-r-sm">
                 <svg class="h-5 w-5 m-auto fill-current" xmlns="http://www.w3.org/2000/svg">
@@ -20,6 +20,10 @@ export default {
         }
     },
 
+    mounted () {
+        this.applyListeners()
+    },
+
     methods: {
         search () {
             if (this.query.trim().length > 0) {
@@ -29,6 +33,22 @@ export default {
                         search_query: this.query
                     }
                 })
+            }
+        },
+
+        applyListeners () {
+            document.addEventListener('keydown', this.keydown)
+        },
+
+        keydown (e) {
+            if (event.target.matches('input')) {
+                return
+            }
+
+            // /
+            if (e.keyCode === 191) {
+                e.preventDefault()
+                this.$refs.search.focus()
             }
         }
     }
