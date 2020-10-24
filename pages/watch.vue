2acca2697498bf418a2866f4887bda08891ac7ef
epndavis <47059262+epndavis@users.noreply.github.com>
2020-10-24 20:18:40 +0100

    feat(share): Add facebook link to share

--- pages/watch.vue
+++ pages/watch.vue
@@ -56,13 +56,13 @@
                                     </div>
                                 </div>
 
-                                <button class="mr-3 px-1 uppercase">
+                                <a :href="`https://www.facebook.com/sharer/sharer.php?u=${$route.fullPath}`" target="_blank" class="mr-3 px-1 uppercase flex items-center">
                                     <svg class="h-6 w-6 fill-current inline-block" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" />
                                     </svg>
 
                                     <span>Share</span>
-                                </button>
+                                </a>
 
                                 <button class="mr-3 px-1 uppercase">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 fill-current inline-block" viewBox="0 0 20 20">
@@ -272,6 +272,11 @@ export default {
                 {
                     name: 'author',
                     content: this.video.channel.name
+                },
+
+                {
+                    property: 'og:url',
+                    content: this.$route.query.page
                 },
 
                 {
