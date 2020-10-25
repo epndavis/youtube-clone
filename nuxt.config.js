d475637b5deac0d422eb5ae23640adf2fe9ca11a
epndavis <47059262+epndavis@users.noreply.github.com>
2020-10-25 14:46:42 +0000

    feat(favicon): Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,16 @@ export default {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'msapplication-TileColor', content: '#da532c' },
+            { name: 'theme-color', content: '#ffffff' }
         ],
         link: [
+            { rel: 'apple-touch-icon', size: '180x180', href: '/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
         ],
         bodyAttrs: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container flex flex-wrap py-5">
-        <div v-for="video in videos" :key="video.id" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-8 sm:mb-12">
+        <div v-for="video in videos" :key="video.id" class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 sm:px-2 mb-8 sm:mb-12">
             <grid-base current="Vertical" :video="video" />
         </div>
     </div>
